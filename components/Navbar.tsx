b34b4b786ff467d61a0ac24eb40b6277307bf1fd
Dom <dominic.vega@avegabros.com>
2023-12-06 14:48:04 +0800

    2nd commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,221 @@
+import React from 'react'
+import Theme from './Theme'
+import { Button } from '@/components/ui/button'
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "@/components/ui/avatar"
+import {
+    Menubar,
+    MenubarCheckboxItem,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+import Link from 'next/link'
+import { RiAdminFill } from "react-icons/ri";
+import { SiWindows11 } from "react-icons/si";
+import { IoMdHome } from "react-icons/io";
+import { FaRegClock } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { Sidebar } from './Sidebar'
+import { FaCartShopping } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineInventory } from "react-icons/md";
+import { MdAccountBalanceWallet } from "react-icons/md";
+import { FaArrowsAlt } from "react-icons/fa";
+import { FiTool } from "react-icons/fi";
+import { BsBarChart } from "react-icons/bs";
+import { HiMiniHandThumbUp } from "react-icons/hi2";
+import { IoIosFlag } from "react-icons/io";
+import { FaFire } from "react-icons/fa";
+import { HiServerStack } from "react-icons/hi2";
+import { BiSolidError } from "react-icons/bi";
+import { BsFillDatabaseFill } from "react-icons/bs";
+import { BsFillSdCardFill } from "react-icons/bs";
+import { FaFilter } from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { IoMdGlobe } from "react-icons/io";
+import { IoMdChatboxes } from "react-icons/io";
+import { IoNotificationsSharp } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='flex justify-start md:justify-end items-center'>
+            <Menubar className='flex flex-row-reverse md:flex-row items-center border-0 my-3'>
+                <Theme/>
+                <MenubarMenu>
+                    <Link href='/'>
+                        <MenubarTrigger className='relative'>
+                            <IoNotificationsOutline size={15}/>
+                            <div className='w-2 h-2 rounded-full bg-customOrange absolute top-1 right-3'></div>
+                        </MenubarTrigger>
+                    </Link>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <div className='hidden md:block'>
+                        <MenubarTrigger className='cursor-pointer'>
+                            <FaRegClock size={15}/>
+                            <p className='hidden md:block text-xs font-normal ml-2'>Nov 30, 2023 (Thu) | 10:16:31 AM</p>
+                        </MenubarTrigger>
+                    </div>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <Link href='/'>
+                        <MenubarTrigger className='cursor-pointer'>
+                            <IoMdHome size={15}/>
+                            <p className='hidden md:block text-xs font-normal ml-2'>Home</p>
+                        </MenubarTrigger>
+                    </Link>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <div className='hidden md:block'>
+                        <MenubarTrigger className=' cursor-pointer'>
+                            <SiWindows11 size={15}/>
+                            <p className='hidden md:block text-xs font-normal ml-2'>Subsystems</p>
+                        </MenubarTrigger>
+                    </div>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <FaEye />
+                            <p className='ml-5'>Manager's Dashboard</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaUser />
+                            <p className='ml-5'>Human Resources</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaCartShopping />
+                            <p className='ml-5'>Purchasing</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <MdOutlineInventory />
+                            <p className='ml-5'>Inventory</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <MdAccountBalanceWallet />
+                            <p className='ml-5'>Accounting</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaArrowsAlt />
+                            <p className='ml-5'>Marketing & Operations</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <BsBarChart />
+                            <p className='ml-5'>Finance</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FiTool />
+                            <p className='ml-5'>Asset Management</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <HiMiniHandThumbUp />
+                            <p className='ml-5'>Compliance</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <IoIosFlag />
+                            <p className='ml-5'>Corporate Services</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaFire />
+                            <p className='ml-5'>IT Services</p>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <div className='hidden md:block'>
+                        <MenubarTrigger className='cursor-pointer'>
+                            <RiAdminFill size={15}/> 
+                            <p className='hidden md:block text-xs font-normal ml-2'>Administrator</p>
+                        </MenubarTrigger>
+                    </div>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <HiServerStack />
+                            <p className='ml-5'>Server Info</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <BiSolidError />
+                            <p className='ml-5'>System Logs</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <BsFillDatabaseFill />
+                            <p className='ml-5'>Master Tables</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <BsFillSdCardFill />
+                            <p className='ml-5'>Database Encoding</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaFilter />
+                            <p className='ml-5'>Database Manual Query</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <FaUserGroup />
+                            <p className='ml-5'>User Accounts</p>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger className=' rounded-full h-12 cursor-pointer'>
+                        <Avatar className=' w-8 h-8'>
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+                            <AvatarFallback>DV</AvatarFallback>
+                        </Avatar>
+                        <h1 className=' font-bold text-customOrange ml-2 text-xs md:hidden'>WELCOME, DOMINIC V.</h1>
+                    </MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <div>
+                                <p>Username: dvega</p>
+                                <p>Email: dominic.vega@vegabros.com</p>
+                                <p>Location: Tayud</p>
+                            </div>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem>
+                            <IoIosMail />
+                            <p className='ml-5'>Webmail</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <IoMdGlobe />
+                            <p className='ml-5'>Website</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <IoMdChatboxes />
+                            <p className='ml-5'>Viber Chatbot</p>
+                        </MenubarItem>
+                        <MenubarItem>
+                            <IoNotificationsSharp />
+                            <p className='ml-5'>Subscribe to SMS Notification</p>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem>
+                            <Button className='w-full h-8'>
+                                <MdLogout />
+                                <p className='ml-5'>Logout</p>
+                            </Button>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+            </Menubar>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/Sidebar.tsx
@@ -0,0 +1,85 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { FiMenu } from "react-icons/fi"
+import { Separator } from "@/components/ui/separator"
+import { Badge } from "@/components/ui/badge"
+
+export function Sidebar() {
+  return (
+    <div className="min-h-screen bg-sidebarColor dark:bg-black">
+      <div className="hidden lg:block space-y-4 w-full">
+        <div className="">
+            <div className="p-5 flex justify-center">
+              <h1 className=" text-2xl font-extrabold text-customOrange">AV ABAS V3</h1>
+            </div>
+            <div className=' flex justify-between items-start p-5'>
+                <div className="">
+                    <p className="mb-2 text-sm font-normal tracking-tight">Welcome!</p>
+                    <p className=" italic font-medium">Dominic Anuta Vega</p>
+                    <Badge>Administrator</Badge>
+                </div>
+                <div className=" cursor-pointer">
+                    <FiMenu size={25}/>
+                </div>
+            </div>
+            <Separator/>
+            <div className="space-y-1">
+                <Button variant="secondary" className="w-full justify-start">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 h-4 w-4"
+                    >
+                    <circle cx="12" cy="12" r="10" />
+                    <polygon points="10 8 16 12 10 16 10 8" />
+                    </svg>
+                    Listen Now
+                </Button>
+                <Button variant="ghost" className="w-full justify-start">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 h-4 w-4"
+                    >
+                    <rect width="7" height="7" x="3" y="3" rx="1" />
+                    <rect width="7" height="7" x="14" y="3" rx="1" />
+                    <rect width="7" height="7" x="14" y="14" rx="1" />
+                    <rect width="7" height="7" x="3" y="14" rx="1" />
+                    </svg>
+                    Browse
+                </Button>
+                <Button variant="ghost" className="w-full justify-start">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 h-4 w-4"
+                    >
+                    <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
+                    <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
+                    <circle cx="12" cy="12" r="2" />
+                    <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
+                    <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
+                    </svg>
+                    Radio
+                </Button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
