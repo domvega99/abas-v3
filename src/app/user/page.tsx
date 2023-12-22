b1106832bbe75ba13c532c1d58680d493f323db6
Dom <dominic.vega@avegabros.com>
2023-12-22 09:25:50 +0800

    mobile responsive

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,35 +3,36 @@ import React, { useEffect, useState } from 'react'
 import TableData from '../../../components/TableData'
 import axios from 'axios';
 import { User } from '../../../types/userTypes';
+import { data } from '../../../api/userData';
 
 const page = () => {
-  const [userData, setUsers] = useState<User[]>([]);
+  // const [userData, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<{ users: User[] }>(
-          'http://localhost:8080/api/users',
-          {
-            withCredentials: true,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
-        setUsers(response.data.users);
-        console.log(response.data.users);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get<{ users: User[] }>(
+  //         'http://localhost:8080/api/users',
+  //         {
+  //           withCredentials: true,
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //           },
+  //         }
+  //       );
+  //       setUsers(response.data.users);
+  //       console.log(response.data.users);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <div className='w-full'>
-        <TableData data={userData}/>
+        <TableData data={data}/>
     </div>
   )
 }
